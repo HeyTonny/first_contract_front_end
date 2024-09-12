6825730012d0dd6d70d44decb95ba85b4d93906b
HeyTonny <jackieking@yandex.ru>
2024-09-12 16:50:07 +0300

    change manifest.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = "https://heytonny.github.io/first_contract_front_end/tonconnect-manifest.json";
+const manifestUrl = "https://heytonny.github.io/first_contract_front_end/manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <TonConnectUIProvider manifestUrl={manifestUrl}>
