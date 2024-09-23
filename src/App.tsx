3afcdaba081fee6fecd0f93466cc6aefe59b025c
HeyTonny <jackieking@yandex.ru>
2024-09-23 14:08:18 +0300

    add showAlert

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,10 @@
-// import "./App.css";
-// import {TonConnectButton} from "@tonconnect/ui-react";
-// import {useMainContract} from "./hooks/useMainContract";
-// import {useTonConnect} from "./hooks/useTonConnect";
-//
-// function App() {
-//     const {sendIncrement} = useMainContract();
-//     const {connected} = useTonConnect()
-//     const {
-//         contract_address,
-//         counter_value,
-//         // recent_sender,
-//         // owner_address,
-//         contract_balance,
-//     } = useMainContract();
-//     return (
-//         <div>
-//             <div>
-//                 <TonConnectButton/>
-//             </div>
-//             <div>
-//                 <div className='Card'>
-//                     <b>Our contract Address</b>
-//                     <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-//                     <b>Our contract Balance</b>
-//                     <div className='Hint'>{contract_balance}</div>
-//                 </div>
-//
-//                 <div className='Card'>
-//                     <b>Counter Value</b>
-//                     <div>{counter_value ?? "Loading..."}</div>
-//                 </div>
-//             </div>
-//             {connected && (
-//                 <a
-//                     onClick={() => {
-//                         sendIncrement();
-//                     }}
-//                 >
-//                     Increment
-//                 </a>
-//             )}
-//         </div>
-//     );
-// }
-//
-// export default App;
-
 import "./App.css";
 import {TonConnectButton} from "@tonconnect/ui-react";
 import {useMainContract} from "./hooks/useMainContract";
 import {useTonConnect} from "./hooks/useTonConnect";
 import {fromNano} from "ton-core";
-import WebApp from "@twa-dev/sdk";
+import WebApp from '@twa-dev/sdk'
+
 
 function App() {
     const {
@@ -63,33 +16,46 @@ function App() {
         sendIncrement,
     } = useMainContract();
     const {connected} = useTonConnect();
-    const showAlert = () => {
-        WebApp.showAllert('Hey there?');
-    };
-
-
-
+    const showAlert = () =>
+    {
+        WebApp.showAlert('Кто здесь!?')
+    }
     return (
         <div>
-            <div>
-                <TonConnectButton/>
-            </div>
-            <div>
-                <div className='Card'>
-                    <b>{WebApp.platform}</b>
-                    <b>Our contract Address</b>
-                    <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-                    <b>Our contract Balance</b>
+            <TonConnectButton/>
+
+            <div className="container">
+                <div>
+                    <h3>Contract Data:</h3>
+                    <b>Our contract Address:</b>
+                    <div className="Hint">{contract_address}</div>
+                    <hr/>
+
+                    <b>Our contract Owner:</b>
+                    <div className="Hint">{owner_address?.toString()}</div>
+                    <hr/>
+
                     {contract_balance && (
-                        <div className='Hint'>{fromNano(contract_balance)}</div>
+                        <>
+                            <b>Our contract Balance:</b>
+                            <div className="Hint">{fromNano(contract_balance)}</div>
+                            <hr/>
+                        </>
                     )}
-                </div>
 
-                <div className='Card'>
-                    <b>Counter Value</b>
-                    <div>{counter_value ?? "Loading..."}</div>
-                </div>
+                    {recent_sender && (
+                        <>
+                            <b>Recent sender:</b>
+                            <div className="Hint">{recent_sender.toString()}</div>
+                            <hr/>
+                        </>
+                    )}
 
+                    <>
+                        <b>Counter Value:</b>
+                        <div>{counter_value ?? "Loading..."}</div>
+                    </>
+                </div>
                 <a
                     onClick={() => {
                         showAlert();
@@ -97,46 +63,22 @@ function App() {
                 >
                     Show Alert
                 </a>
+                <div>
+                    <h3>Contract actions: </h3>
+                    {connected ? (
+                        <>
+                            <p>Increment contract balance by 1 TON, with 0.05 TON as a comission</p>
+                            <button onClick={sendIncrement}>Increment</button>
+                            <hr/>
 
-                <br/>
-
-                {connected && (
-                    <a
-                        onClick={() => {
-                            sendIncrement();
-                        }}
-                    >
-                        Increment by 5
-                    </a>
-                )}
-
-                <br/>
-
-                {connected && (
-                    <a
-                        onClick={() => {
-                            sendDeposit();
-                        }}
-                    >
-                        Request deposit of 1 TON
-                    </a>
-                )}
-
-                <br/>
-
-                {connected && (
-                    <a
-                        onClick={() => {
-                            sendWithdrawalRequest();
-                        }}
-                    >
-                        Request 0.7 TON withdrawal
-                    </a>
-                )}
+                        </>
+                    ) : (
+                        <p>Connect wallet to start action</p>
+                    )}
+                </div>
             </div>
         </div>
     );
 }
 
 export default App;
-
